30cf3d162ac1492294cb3238f87d8e30765669e4
wearesuite <dearste@gmail.com>
2014-06-16 16:03:50 +0200

    Create View.php

---
+++ framework/src/View.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+class View{
+
+
+
+	public static function make($view, $data_array = array())
+    {
+        // load Twig, the template engine
+        // @see http://twig.sensiolabs.org
+        $twig_loader = new Twig_Loader_Filesystem(PATH_VIEWS);
+        $twig = new Twig_Environment($twig_loader);
+		
+		//$twig->addFunction('parseText', new Twig_Function_Function('parseText'));
+		//$twig->addFunction('unserializeArray', new Twig_Function_Function('unserializeArray'));
+
+
+        // render a view while passing the to-be-rendered data
+        return $twig->render($view . PATH_VIEW_FILE_TYPE, $data_array);
+    }
+
+}
